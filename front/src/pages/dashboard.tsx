fd0602fa86dc25a9f7456cf6fa15c003a8f6b117
Thonio <anthonio.rj@hotmail.fr>
2026-03-12 14:49:13 +0300

    fix error CORS

--- front/src/pages/dashboard.tsx
+++ front/src/pages/dashboard.tsx
@@ -1,15 +1,27 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import type { Stats } from "../api/type";
+import { getStats } from "../api/campaigns";
 
 export default function Dashboard() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState<Stats | null>(null)
   const [loading, setLoading] = useState(true)
 
   const cards = [
-    { label: 'Total Campaigns', value: 2 },
-    { label: 'Active Campaigns', value: 4 },
-    { label: 'Total Impressions', value: 6 },
-    { label: 'Top Advertiser', value: 8 }
+    { label: 'Total Campaigns', value: data?.totalCampaigns },
+    { label: 'Active Campaigns', value: data?.activeCampaigns },
+    { label: 'Total Impressions', value: data?.totalImpressions },
+    { label: 'Top Advertiser', value: data?.topAdvertiser }
   ];
+
+  useEffect(() => {
+    getStats()
+      .then(setData)
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false))
+  }, [])
+
+  if (loading) return <p className="p-6">Cahrgement...</p>;
+  if (!data) return <p className="p-6 text-red-500">Erreur de chargement des donnees.</p>;
 
   return (
     <div className="p-6">
